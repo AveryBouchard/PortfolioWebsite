5db018a8c3bb0d72c53d46228944ade69e6638ca
Avery Bouchard <averybou@gmail.com>
2020-05-30 22:50:51 -0400

    Added Blog

--- jobs/models.py
+++ jobs/models.py
@@ -5,8 +5,3 @@ class Job(models.Model):
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
 
-
-class Blog(models.Model):
-    image = models.ImageField(upload_to='images/')
-    summary = models.CharField(max_length=200)
-    post = models.CharField()
